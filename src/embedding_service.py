78fa7d50dbc7ca66c835d1676aa30246e36da8a1
Hollmann Kevin <101499108+khollmes@users.noreply.github.com>
2026-01-06 11:11:27 +0100

    Remove engine_args setup in embedding_service.py
    Removed engine_args configuration for embedding service.

--- src/embedding_service.py
+++ src/embedding_service.py
@@ -3,17 +3,6 @@ import os
 os.environ["INFINITY_DISABLE_OPTIMUM"] = "TRUE" 
 from infinity_emb.engine import AsyncEngineArray, EngineArgs
 
-engine_args.append(
-    EngineArgs(
-        model_name_or_path=model_name,
-        batch_size=batch_size,
-        engine=self.config.backend,   
-        dtype=dtype,
-        model_warmup=False,
-        lengths_via_tokenize=True,
-        bettertransformer=False,      
-    )
-)
 from utils import (
     OpenAIModelInfo,
     ModelInfo,
